31b32db98109872f7eb8acaea6e257d663e5c06d
Abdullah8006 <abeiy4u@gmail.com>
2017-10-23 17:43:21 +0300

    Removed deleteanytime folder from git tracking.

--- src/main/java/com/appster/abdullah/util/deleteanytime/Generics/Garage.java
+++
@@ -1,9 +0,0 @@
-package com.appster.abdullah.util.deleteanytime.Generics;
-
-public interface Garage<T> {
-    
-    public void parkVehicle(T t);
-    
-    public T moveOut(T t);
-    
-}
--- src/main/java/com/appster/abdullah/util/deleteanytime/Generics/Vehicle.java
+++
@@ -1,9 +0,0 @@
-package com.appster.abdullah.util.deleteanytime.Generics;
-
-public interface Vehicle {
-    
-    public int getChasis();
-    
-    public String getName();
-    
-}
